e0c16874b25274bc2251e6098e95962c89d6af7b
Winlypse <nts3@o2.pl>
2024-10-22 15:00:56 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noMobLoot/NoMobLoot.kt
@@ -0,0 +1,14 @@
+package winlyps.noMobLoot
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoMobLoot : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
